686cbc079433d9747ad25bd04747ceed1419e5cd
SamYuan1990 <yy19902439@126.com>
2020-11-27 19:50:32 +0800

    1st commit
    Signed-off-by: SamYuan1990 <yy19902439@126.com>

---
+++ config.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+curl -vsS https://raw.githubusercontent.com/hyperledger/fabric/release-2.2/scripts/bootstrap.sh | bash
+cd ./fabric-samples/test-network
+echo y |  ./network.sh down -i 2.2
+echo y |  ./network.sh up createChannel -i 2.2
+echo y |  ./network.sh deployCC
