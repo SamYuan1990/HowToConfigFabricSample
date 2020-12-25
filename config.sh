7758ace65029f9ab74e35dd80986d2c9c5d357b9
SamYuan1990 <yy19902439@126.com>
2020-12-25 20:06:27 +0800

    fix up
    Signed-off-by: SamYuan1990 <yy19902439@126.com>

--- config.sh
+++ config.sh
@@ -4,4 +4,4 @@ curl -vsS https://raw.githubusercontent.com/hyperledger/fabric/release-2.2/scrip
 cd ./fabric-samples/test-network
 echo y |  ./network.sh down -i 2.2
 echo y |  ./network.sh up createChannel -i 2.2
-echo y |  ./network.sh deployCC
+echo y |  ./network.sh deployCC -ccn basic -ccp ../asset-transfer-basic/chaincode-go/ -ccl go
